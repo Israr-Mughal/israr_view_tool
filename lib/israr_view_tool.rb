ff466bed7cdddeafc0830211f8b55b695dff747a
Israr <https://Israr_Shahid@bitbucket.org>
2019-09-28 11:51:21 +0500

    Dev tool inital commit

---
+++ lib/israr_view_tool.rb
@@ -0,0 +1,7 @@
+require "israr_view_tool/version"
+require "israr_view_tool/renderer"
+
+module IsrarViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/israr_view_tool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe IsrarViewTool do
+  it "has a version number" do
+    expect(IsrarViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
